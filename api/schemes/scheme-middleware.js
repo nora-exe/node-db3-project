7ed5582f5c7617d484715aa64bd70811ecc0e763
Nora C <nocorser@gmail.com>
2021-07-14 17:16:08 -0500

    middleware

--- api/schemes/scheme-middleware.js
+++ api/schemes/scheme-middleware.js
@@ -1,58 +1,45 @@
-const Scheme = require('../schemes/scheme-model');
+const db = require('../../data/db-config.js');
 
-/*
-  If `scheme_id` does not exist in the database:
-
-  status 404
-  {
-    "message": "scheme with scheme_id <actual id> not found"
-  }
-*/
-const checkSchemeId = (req, res, next) => {
-  const scheme = Scheme.findById(req.params.id)
-  if (!scheme) {
-    res.status(404).json({
-      message: "scheme with scheme_id <actual id> not found"
-    })
+const checkSchemeId = async (req, res, next) => {
+  try {
+    const exists = await db('schemes')
+      .where('scheme_id', req.params.scheme_id)
+      .first()
+      if (!exists) {
+        next({
+          status: 404,
+          message: `scheme with scheme_id ${req.params.scheme_id} not found`
+        })
+      } else {
+        next()
+      }
+  } catch (err) {
+    next(err)
   }
 }
 
-/*
-  If `scheme_name` is missing, empty string or not a string:
-
-  status 400
-  {
-    "message": "invalid scheme_name"
-  }
-*/
 const validateScheme = (req, res, next) => {
-  if (!req.scheme_name || !req.scheme_name.trim()) {
-    res.status(400).json({ message: 'invalid scheme_name' })
+  const scheme_name = req.body
+  if (!scheme_name || !scheme_name.trim()) {
+    next ({
+      status: 400,
+      message: 'invalid scheme_name'
+    })
   } else {
-    req.scheme_name = req.scheme_name.trim()
     next()
   }
 }
 
-/*
-  If `instructions` is missing, empty string or not a string, or
-  if `step_number` is not a number or is smaller than one:
-
-  status 400
-  {
-    "message": "invalid step"
-  }
-*/
 const validateStep = (req, res, next) => {
-const instructions = req.body
-if (!instructions || !instructions.trim() || !isNaN(step_number) || step_number < 1) {
-  res.status(400).json({
-    message: 'invalid step'
-  })
-} else {
-  req.instructions = instructions.trim()
-  next()
-}
+  const { instructions, step_number } = req.body
+  if (!instructions || !instructions.trim() || !isNaN(step_number) || step_number < 1) {
+    next({
+      status: 400,
+      message: 'invalid step'
+    })
+  } else {
+    next()
+  }
 }
 
 module.exports = {
